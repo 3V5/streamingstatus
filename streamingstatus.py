2ad001e44c2e1192ef7469ee378d8b27455a1eea
Sleep e <80415229+3V5@users.noreply.github.com>
2021-06-10 12:05:53 -0700

    Update streamingstatus.py

--- streamingstatus.py
+++ streamingstatus.py
@@ -1,18 +1,13 @@
-import json
 import discord
-from discord.ext import commands
-from discord.ext import tasks
 
-with open(f"config.json") as f:
-        configmain = json.load(f)
-        token = configmain.get('token')
-        title = configmain.get('title')
-        url = configmain.get('url')
-        client = discord.Client()
+client = discord.Client()
+token = "Discord Token"
+title = "Hello hi hi"
+url = "https://twitch.tv/meowcat"
 
 @client.event
 async def on_ready():
-    print(f"    started\n    Discord: Discord.gg/cdKzZQJH6c\n    Title: {title}\n    Url:   {url}")
+    print(f"    started\n    Title: {title}\n    Url:   {url}")
     await client.change_presence(activity=discord.Streaming(name=f"{title}", url=f"{url}"))
 
 client.run(token, bot=False)
